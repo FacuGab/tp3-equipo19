70fc6cc19f2014d3054399e0622f28dfc3904e74
Facundo Amarilla <facundogabriel91@gmail.com>
2023-05-26 20:16:33 -0300

    Primer Commit

---
+++ CarritoWeb/Vistas/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CarritoWeb.Vistas.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <!-- LISTA -->
+            <asp:GridView ID="dgvListaPrincipal" CssClass="table table-bordered table-dark" runat="server">
+            </asp:GridView>
+        </div>
+        <div class="col-3"></div>
+    </div>
+
+</asp:Content>
---
+++ CarritoWeb/Vistas/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CarritoWeb.Vistas
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private NegocioArticulo negocio;
+        private Articulo articulo;
+        private List<Articulo> listaArticulos;
+        //TODO: LOAD
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                negocio = new NegocioArticulo();
+                listaArticulos = negocio.Leer();
+
+            }
+            dgvListaPrincipal.DataSource = listaArticulos;
+            dgvListaPrincipal.DataBind();
+        }
+    }//fin
+}
