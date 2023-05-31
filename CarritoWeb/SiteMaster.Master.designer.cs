2ce45a2cc3b5c097c796194265525a54b80546a5
Emanuel Casulo <emanuel.casulo@alumnos.frgp.utn.edu.ar>
2023-05-31 19:05:37 -0300

    actualizaciones

--- CarritoWeb/SiteMaster.Master.designer.cs
+++ CarritoWeb/SiteMaster.Master.designer.cs
@@ -58,14 +58,5 @@ namespace CarritoWeb
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblTotalCarrito;
-
-        /// <summary>
-        /// dgvCarrito control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView dgvCarrito;
     }
 }
